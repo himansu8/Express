1b6772205fc5a779235c28a3a88d660553716e16
himansu8 <himansusekharsahu50@gmail.com>
2023-12-25 18:38:55 +0530

    express get and post

--- express_basic/server.js
+++ express_basic/server.js
@@ -1,20 +1,52 @@
 const express = require('express')
 const app = express()
 const port = 3000
-
+app.use(express.json());    //middleware
+//-----------------------------------------------------------------------
 app.get('/', (req, res) => {
-    console.log(req)
+  console.log(req.url)
+  console.log(req.method)
+  console.log(req.headers)
   res.send('Hello World!!!!')
 })
 
 app.get('/todo', (req, res) => {
-    res.send('Hello todo')
-  })
+  res.send('Hello todo')
+})
 
-  app.get('/users', (req, res) => {
-    res.send('Hello users')
-  })
+app.get('/users', (req, res) => {
+  res.send('Hello users')
+})
+//-----------------------------------------------------------------------
+app.post('/comment', (req, res) => {
+  console.log(req.body)
+  res.send('comment post successfully')
+})
 
+app.post('/commentquery', (req, res) => {
+  console.log(req.body)
+  console.log(req.query)
+  res.send('query post successfully')
+})
+
+app.post('/commentParams/:id', (req, res) => {
+  //console.log(req.body)
+  console.log(req.params)
+  res.send('params post successfully')
+})
+//------------------------------------------------------------------------
+app.get('/users/:user', (req, res) => {
+  console.log(req.params)
+  console.log(req.body)
+  res.send(`this is the profile of ${req.params.user}`)
+})
+//------------------------------------------------------------------------
+app.get('/add/:n1/:n2', (req, res) => {
+  console.log(req.params)
+  let sum= Number(req.params.n1)+Number(req.params.n2)
+  res.send(`this sum is  ${sum}`)
+})
+//------------------------------------------------------------------------ 
 app.listen(port, () => {
   console.log(`Example app listening on port ${port}`)
-})
+})     
