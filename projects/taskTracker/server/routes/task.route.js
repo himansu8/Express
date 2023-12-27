bdb8a89e66b2ffd672d1678cf7614d6c173068ff
himansu8 <himansusekharsahu50@gmail.com>
2023-12-27 14:09:36 +0530

    middleware added

--- projects/taskTracker/server/routes/task.route.js
+++ projects/taskTracker/server/routes/task.route.js
@@ -5,8 +5,44 @@ import authMiddleware from '../middlewares/auth/veriftToken.js';
 
 const router = express.Router();
 
-
+/*
+description: create a single task
+method :post
+api_url: api/task
+*/
 router.post('/',authMiddleware, createTask);
+
+/*
+description: get a all task
+method :get
+api_url: api/task
+*/
+router.get('/',authMiddleware );
+
+
+/*
+description: get a single task
+method :get
+api_url: api/task/:taskid
+*/
+router.get('/:taskid',authMiddleware);
+
+
+/*
+description: update a single task
+method :patch
+api_url: api/task/:taskid
+*/
+router.patch('/:taskid',authMiddleware);
+
+/*
+description: delete a single task
+method :delete
+api_url: api/task/:taskid
+*/
+router.delete('/:taskid',authMiddleware);
+
+
 
 
 export default router;
